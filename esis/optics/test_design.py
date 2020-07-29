dc409483fad12dd88959eea10b387769d33184a9
Roy Smart <roytsmart@gmail.com>
2020-07-29 14:23:59 -0600

    - refactor test names.

--- esis/optics/test_design.py
+++ esis/optics/test_design.py
@@ -2,7 +2,7 @@ import kgpy.optics
 from . import design
 
 
-def test_esis_as_designed():
-    esis = design(pupil_samples=5, field_samples=5)
+def test_final():
+    esis = design.final(pupil_samples=5, field_samples=5)
     assert isinstance(esis.system, kgpy.optics.System)
     assert isinstance(esis.system.image_rays, kgpy.optics.Rays)
