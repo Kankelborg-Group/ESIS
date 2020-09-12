fixed problems with dn_to_photon @byrdie where we were attempting to reassign single elements of a u.Quantity with different units.
Data prep question, did we subtract off the read noise when correcting bias?

esis/data/level_0.py
@@ -16,7 +16,7 @@ import esis
 
 @dataclasses.dataclass
 class Level_0:
-    data: np.ndarray
+    data: u.Quantity
     time: astropy.time.Time
     cam_id: u.Quantity
     cam_sn: np.ndarray
@@ -58,7 +58,7 @@ class Level_0:
             for c in range(num_channels):
                 hdu = astropy.io.fits.open(fits_list[bad_exposures + i, c])[0]
                 header = hdu.header
-                self.data[i, c] = hdu.data
+                self.data[i, c] = hdu.data * u.ct
                 self.time[i, c] = astropy.time.Time(header['IMG_TS'])
                 self.cam_id[i, c] = int(header['CAM_ID'][~0]) * u.chan
                 self.cam_sn[i, c] = int(header['CAM_SN'])
@@ -84,7 +84,7 @@ class Level_0:
     def zeros(cls, shape: typ.Sequence[int]):
         sh = shape[:2]
         return cls(
-            data=np.zeros(shape),
+            data=np.zeros(shape) * u.ct,
             time=astropy.time.Time(np.zeros(sh), format='unix'),
             cam_id=np.zeros(sh, dtype=np.int) * u.chan,
             cam_sn=np.zeros(sh, dtype=np.int),

esis/optics/components/detector/detector.py
@@ -168,18 +168,18 @@ class Detector(optics.component.CylindricalComponent[SurfaceT]):
         """
         For a given wavelength return a detector size array with units of photon / DN
         """
-        data = data.copy()
+        photon_data = data.value * u.photon
+
         channel_gains = self.gain
         quadrants = self.quadrants
-        photon_energy = const.c * const.h / wavelength
+        photon_energy = const.c * const.h / wavelength / u.photon
         Si_e_hole_pair = 3.6 * u.eV / u.electron
 
         print(channel_gains)
         for channel,channel_gain in enumerate(channel_gains):
             for i, quad in enumerate(quadrants):
-                data[(...,channel)+quad] *= channel_gain[i] * Si_e_hole_pair / photon_energy
-
-        return data
+                photon_data[(...,channel)+quad] = data[(...,channel)+quad] * channel_gain[i] * Si_e_hole_pair / photon_energy
+        return photon_data
 
     def remove_inactive_pixels(self, frames: np.ndarray, axis: int = ~0):
         frames = self.remove_overscan_pixels(frames, ccd_long_axis=axis)