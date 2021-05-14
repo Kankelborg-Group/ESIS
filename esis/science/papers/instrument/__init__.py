4830db93b2590dfa16e3fcdbfa0f88ecfa50dee9
Roy Smart <roytsmart@gmail.com>
2021-05-14 00:01:37 -0600

    Added author list to instrument paper.

--- esis/science/papers/instrument/__init__.py
+++ esis/science/papers/instrument/__init__.py
@@ -18,7 +18,70 @@ def document() -> pylatex.Document:
     )
 
     doc.append(pylatex.Command('title', 'The EUV Snapshot Imaging Spectrograph'))
-    doc.append(pylatex.Command('author', 'Roy Smart'))
+
+    str_msu = 'Montana State University, Department of Physics, P.O. Box 173840, Bozeman, MT 59717, USA'
+    affil_msu = pylatex.Command('affiliation', str_msu)
+
+    str_msfc = 'NASA Marshall Space Flight Center, Huntsville, AL 35812, USA'
+    affil_msfc = pylatex.Command('affiliation', str_msfc)
+
+    str_lbnl = 'Lawrence Berkeley National Laboratory, 1 Cyclotron Road, Berkeley, CA 94720, USA'
+    affil_lbnl = pylatex.Command('affiliation', str_lbnl)
+
+    str_rxo = 'Reflective X-ray Optics LLC, 425 Riverside Dr., #16G, New York, NY 10025, USA'
+    affil_rxo = pylatex.Command('affiliation', str_rxo)
+
+
+    doc.append(pylatex.Command('author', 'Hans T. Courrier'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'Roy T. Smart'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'Charles C. Kankelborg'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'Amy R. Winebarger'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Ken Kobayashi'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Brent Beabout'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Dyana Beabout'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Ben Carrol'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'Jonathan Cirtain'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'James A. Duffy'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Carlos Gomez'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Eric Gullikson'))
+    doc.append(affil_lbnl)
+
+    doc.append(pylatex.Command('author', 'Micah Johnson'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'Jacob D. Parker'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'Laurel Rachmeler'))
+    doc.append(affil_msfc)
+
+    doc.append(pylatex.Command('author', 'Larry Springer'))
+    doc.append(affil_msu)
+
+    doc.append(pylatex.Command('author', 'David L. Windt'))
+    doc.append(affil_rxo)
 
     with doc.create(pylatex.Section('Introduction')):
         pass
