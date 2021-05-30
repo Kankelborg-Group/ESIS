f1c2e81529e964c76efd118f19e33578601e75b9
Roy Smart <roytsmart@gmail.com>
2021-05-29 23:32:29 -0600

    Added the start of the 'Science Objectives' section to the instrument paper.

--- esis/science/papers/instrument/__init__.py
+++ esis/science/papers/instrument/__init__.py
@@ -533,7 +533,32 @@ dispersion planes, and a field stop."""
             ))
 
     with doc.create(pylatex.Section('Science Objectives')):
-        pass
+        doc.append(pylatex.NoEscape(
+            r"""The previous section discussed the qualitative design aspects of \ESIS\ learned from experience with the 
+\MOSES\ instrument.  
+\MOSES, in turn, demonstrated a working concept of simultaneous \EUV\ imaging and spectroscopy.
+This concept adds a unique capability to the science that we can obtain from the \EUV\ solar atmosphere.
+\ESIS, sharing the same payload volume as \MOSES, is manifested to fly in 2019.
+In this section, we set forth specific scientific objectives for the combined \ESIS/\MOSES mission.
+From these objectives, and with an eye toward synergistic operation of \MOSES\ and \ESIS, in \S\,\ref{SS-SCIREQ} we 
+derive the quantitative science requirements that drive the \ESIS\ design.
+
+The combined \ESIS/\MOSES mission will address the following two overarching science goals: \begin{inparaenum}[(1)] 
+\item observe magnetic reconnection in the \TR\label{item-goal1}, and \item map the transfer of energy through the \TR\ 
+with emphasis on \MHD\ waves\label{item-goal2}. \end{inparaenum}
+These objectives have significant overlap with the missions of \IRIS~\citep{IRIS14}, the \EIS~\citep{Culhane07}
+aboard Hinode, the \EUNIS~\citep{Brosius07,Brosius14}, and a long history of FUV and EUV slit spectrographs.
+The \ESIS\ instrument, however, can obtain both spatial and spectral information co-temporally.
+This will allow us to resolve complicated morphologies of compact \TR\ reconnection events (as was done with 
+\MOSES~\citep{Fox11,Rust17,Courrier18}) and observe signatures of \MHD\ waves over a large portion of the solar disk.
+Therefore, in support of goal~\ref{item-goal1}, we will use \ESIS\ and \MOSES\ to map flows as a function of time and 
+space in multiple \TR\ reconnection events.
+To achieve goal~\ref{item-goal2}, we will cross-correlate the evolution at multiple temperatures in the \TR\ to map the 
+vertical transport of energy over a wide \FOV.
+In the latest configuration, the \MOSES\ optics are optimized around Ne\,\textsc{vii} (\SI{0.5}{\mega\kelvin}).
+To achieve our goals, \ESIS\ should have a complementary wavelength choice such that we can observe a reasonably 
+isolated emission line formed in the lower \TR."""
+        ))
 
     with doc.create(pylatex.Section('The ESIS Instrument')):
         pass
