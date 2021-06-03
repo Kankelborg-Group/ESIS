8ac05ad63d40a739c6d7003a3f6c67c86efae199
Roy Smart <roytsmart@gmail.com>
2021-06-03 14:01:28 -0600

    Added a Detector.quantum_efficiency parameter.

--- esis/optics/design.py
+++ esis/optics/design.py
@@ -209,6 +209,7 @@ def final(
     detector.gain = 1 * u.electron / u.adu
     detector.readout_noise = 4 * u.adu
     detector.exposure_length_min = 1.2 * u.s
+    detector.quantum_efficiency = 33 * u.percent
     if all_channels:
         detector.plot_kwargs['linestyle'] = dashstyle_channels
 
