c2de811bab29f1f58cd7aae547c6d4ca1612e221
Roy Smart <roytsmart@gmail.com>
2021-06-03 13:42:04 -0600

    Added Filter.thickness_oxide and Filter.mesh_ratio parameters.

--- esis/optics/filter.py
+++ esis/optics/filter.py
@@ -17,6 +17,8 @@ class Filter(optics.component.CylindricalComponent[SurfT]):
     clear_radius: u.Quantity = 0 * u.mm
     border_width: u.Quantity = 0 * u.mm
     thickness: u.Quantity = 0 * u.mm
+    thickness_oxide: u.Quantity = 0 * u.mm
+    mesh_ratio: u.Quantity = 100 * u.percent
 
     @property
     def clear_diameter(self) -> u.Quantity:
@@ -37,6 +39,10 @@ class Filter(optics.component.CylindricalComponent[SurfT]):
         surface.aperture_mechanical = optics.surface.aperture.Circular(
             radius=self.clear_radius + self.border_width,
         )
+        surface.material = optics.surface.material.AluminumThinFilm(
+            thickness=self.thickness,
+            mesh_ratio=self.mesh_ratio,
+        )
         return surface
 
     def copy(self) -> 'Filter':
@@ -44,6 +50,9 @@ class Filter(optics.component.CylindricalComponent[SurfT]):
         other.inclination = self.inclination.copy()
         other.clear_radius = self.clear_radius.copy()
         other.border_width = self.border_width.copy()
+        other.thickness = self.thickness.copy()
+        other.thickness_oxide = self.thickness_oxide.copy()
+        other.mesh_ratio = self.mesh_ratio.copy()
         return other
 
     @property
@@ -52,4 +61,7 @@ class Filter(optics.component.CylindricalComponent[SurfT]):
         dataframe['inclination'] = [format.quantity(self.inclination.to(u.deg))]
         dataframe['clear radius'] = [format.quantity(self.clear_radius.to(u.mm))]
         dataframe['border width'] = [format.quantity(self.border_width.to(u.mm))]
+        dataframe['thickness'] = [format.quantity(self.thickness.to(u.nm))]
+        dataframe['oxide thickness'] = [format.quantity(self.thickness_oxide.to(u.nm))]
+        dataframe['mesh ratio'] = [format.quantity(self.mesh_ratio.to(u.percent))]
         return dataframe
