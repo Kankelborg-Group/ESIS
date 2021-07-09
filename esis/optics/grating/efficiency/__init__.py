1b72ed97d330a226d556a67e7f8912bc84d83c67
Roy Smart <roytsmart@gmail.com>
2021-07-08 20:36:35 -0600

    Updated functions in optics.grating.efficiency to provide more information about grid parameters

--- esis/optics/grating/efficiency/__init__.py
+++ esis/optics/grating/efficiency/__init__.py
@@ -21,40 +21,49 @@ file_vs_position_y = base_path / 'mul063282.txt'
 file_vs_wavelength = base_path / 'mul063315.abs'
 
 
-def vs_angle_at_0aoi() -> typ.Tuple[u.Quantity, u.Quantity]:
+def vs_angle_at_0aoi() -> typ.Tuple[u.Quantity, u.Quantity, u.Quantity, u.Quantity]:
     df = pandas.read_table(file_vs_angle_at_0aoi)
+    angle_input = 0 * u.deg
     angle_output = df.index.to_numpy() * u.deg
+    wavelength = 63 * u.nm
     efficiency = 100 * df.to_numpy() * u.percent
-    return angle_output, efficiency
+    return angle_input, angle_output, wavelength, efficiency
 
 
-def vs_angle_at_3aoi() -> typ.Tuple[u.Quantity, u.Quantity]:
+def vs_angle_at_3aoi() -> typ.Tuple[u.Quantity, u.Quantity, u.Quantity, u.Quantity]:
     df = pandas.read_table(file_vs_angle_at_3aoi)
+    angle_input = 3 * u.deg
     angle_output = df.index.to_numpy() * u.deg
+    wavelength = 63 * u.nm
     efficiency = 100 * df.to_numpy() * u.percent
-    return angle_output, efficiency
+    return angle_input, angle_output, wavelength, efficiency
 
 
-def vs_position_x() -> typ.Tuple[u.Quantity, u.Quantity]:
+def vs_position_x() -> typ.Tuple[u.Quantity, u.Quantity, u.Quantity, u.Quantity]:
     df = pandas.read_table(file_vs_position_x, sep=' ')
     arr = df.to_numpy()
     position_x = arr[..., 0] * u.mm
+    position_y = 0 * u.mm
+    wavelength = 63 * u.nm
     efficiency = 100 * arr[..., 1] * u.percent
-    return position_x, efficiency
+    return position_x, position_y, wavelength, efficiency
 
 
-def vs_position_y() -> typ.Tuple[u.Quantity, u.Quantity]:
+def vs_position_y() -> typ.Tuple[u.Quantity, u.Quantity, u.Quantity, u.Quantity]:
     df = pandas.read_table(file_vs_position_y)
     arr = df.to_numpy()
+    position_x = 0 * u.mm
     position_y = arr[..., 0] * u.mm
+    wavelength = 63 * u.nm
     efficiency = 100 * arr[..., 1] * u.percent
-    return position_y, efficiency
+    return position_x, position_y, wavelength, efficiency
 
 
-def vs_wavelength() -> typ.Tuple[u.Quantity, u.Quantity]:
+def vs_wavelength() -> typ.Tuple[u.Quantity, u.Quantity, u.Quantity]:
     df = pandas.read_table(file_vs_wavelength, sep=' ')
     arr = df.to_numpy()
+    angle_input = 0 * u.deg
     wavelength = arr[..., 0] * u.nm
     efficiency = 100 * arr[..., 1] * u.percent
-    return wavelength, efficiency
+    return angle_input, wavelength, efficiency
 
--- esis/optics/grating/efficiency/witness/__init__.py
+++ esis/optics/grating/efficiency/witness/__init__.py
@@ -15,23 +15,24 @@ file_vs_wavelength_g19 = base_path / 'Witness_g19.txt'
 file_vs_wavelength_g24 = base_path / 'Witness_g24.txt'
 
 
-def _vs_wavelength(file: pathlib.Path) -> typ.Tuple[str, u.Quantity, u.Quantity]:
+def _vs_wavelength(file: pathlib.Path) -> typ.Tuple[str, u.Quantity, u.Quantity, u.Quantity]:
     df = pandas.read_table(file, sep=' ')
     serial_number = df.columns[~0]
     arr = df.to_numpy()
+    angle_input = 4 * u.deg
     wavelength = arr[..., 0] * u.nm
     efficiency = 100 * arr[..., 1] * u.percent
-    return serial_number, wavelength, efficiency
+    return serial_number, angle_input, wavelength, efficiency
 
 
-def vs_wavelength_g17() -> typ.Tuple[str, u.Quantity, u.Quantity]:
+def vs_wavelength_g17() -> typ.Tuple[str, u.Quantity, u.Quantity, u.Quantity]:
     return _vs_wavelength(file_vs_wavelength_g17)
 
 
-def vs_wavelength_g19() -> typ.Tuple[str, u.Quantity, u.Quantity]:
+def vs_wavelength_g19() -> typ.Tuple[str, u.Quantity, u.Quantity, u.Quantity]:
     return _vs_wavelength(file_vs_wavelength_g19)
 
 
-def vs_wavelength_g24() -> typ.Tuple[str, u.Quantity, u.Quantity]:
+def vs_wavelength_g24() -> typ.Tuple[str, u.Quantity, u.Quantity, u.Quantity]:
     return _vs_wavelength(file_vs_wavelength_g24)
 
