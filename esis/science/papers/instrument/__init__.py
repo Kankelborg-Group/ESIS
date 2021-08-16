ca20ca4a5f3e43a6254fb5f1ba2f082017791572
Roy Smart <roytsmart@gmail.com>
2021-08-16 11:46:15 -0600

    Added the \DACS acronym macro to the instrument paper.

--- esis/science/papers/instrument/__init__.py
+++ esis/science/papers/instrument/__init__.py
@@ -769,6 +769,7 @@ def document() -> kgpy.latex.Document:
     doc.preamble.append(kgpy.latex.Acronym('LBNL', 'Lawrence Berkley National Laboratory'))
     doc.preamble.append(kgpy.latex.Acronym('MSFC', 'Marshall Space Flight Center', short=True))
     doc.preamble.append(kgpy.latex.Acronym('VR', pylatex.NoEscape('\citet{Vernazza78}'), pylatex.NoEscape('V\&R')))
+    doc.preamble.append(kgpy.latex.Acronym('DACS', 'data acquisition and control system'))
 
     doc.preamble.append(kgpy.latex.Acronym('SiC', 'silicon carbide', short=True))
     doc.preamble.append(kgpy.latex.Acronym('Al', 'aluminum', short=True))
@@ -2085,7 +2086,7 @@ Two \roy{\DetectorNumOverscanColumnWords} overscan columns on either side of the
 noise and dark current.
 When the camera receives the trigger signal, it transfers the image from the imaging region to the storage regions and 
 starts image readout.
-The digitized data are sent to the Data Acquisition and Control System (DACS) through a SpaceWire interface immediately, 
+The digitized data are sent to the \DACS\ through a SpaceWire interface immediately, 
 one line at a time.
 The frame transfer takes $<$\SI{60}{\milli\second}, and readout takes \SI{1.1}{\second}.
 The cadence is adjustable from 2-\SI{600}{\second} in increments of \SI{100}{\milli\second}, to satisfy the requirement 
@@ -2188,9 +2189,9 @@ Camera & Quad & Gain & Read Noise & Dark Current \\
         with doc.create(pylatex.Subsection('Avionics')):
             doc.append(pylatex.NoEscape(
                 r"""
-The \ESIS\ DACS are based on the designs used for both \CLASP~\citep{Kano12,Kobayashi12} and \HiC~\citep{Kobayashi2014}.
+The \ESIS\ \DACS\ are based on the designs used for both \CLASP~\citep{Kano12,Kobayashi12} and \HiC~\citep{Kobayashi2014}.
 The electronics are a combination of Military Off-The-Shelf (MOTS) hardware and custom designed components.
-The DACS is a 6-slot, 3U, open VPX PCIe architecture conduction cooled system using an AiTech C873 single board
+The \DACS\ is a 6-slot, 3U, open VPX PCIe architecture conduction cooled system using an AiTech C873 single board
 computer.
 The data system also include a MOTS PCIe switch card, \MSFC\ parallel interface card, and two MOTS Spacewire cards.
 A slot for an additional Spacewire card is included to accommodate two more cameras for the next \ESIS\ flight.
@@ -2198,10 +2199,10 @@ The C873 has a \SI{2.4}{\giga\hertz} Intel i7 processor with \SI{16}{\giga b} of
 The operating temperature range for the data system is -40 to +85 C.
 The operating system for the flight data system is Linux Fedora 23.
 
-The DACS is responsible for several functions;
+The \DACS\ is responsible for several functions;
 it controls the \ESIS\ experiment, responds to timers and uplinks, acquires and stores image data from the cameras, 
 downlinks a subset of images through telemetry, and provides experiment health and status.
-The DACS is housed with the rest of the avionics (power supply, analog signal conditioning system) in a 
+The \DACS\ is housed with the rest of the avionics (power supply, analog signal conditioning system) in a 
 0.56-\SI{0.43}{\meter} transition section outside of the experiment section.
 This relaxes the thermal and cleanliness constraints placed on the avionics.
 Custom DC/DC converters are used for secondary voltages required by other electronic components.
