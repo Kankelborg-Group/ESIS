1c4069fc0bc401f3b7733fb07c126fb705beb899
Roy Smart <roytsmart@gmail.com>
2021-08-25 10:36:22 -0600

    Disable stdout capture on instrument paper test.

--- esis/science/papers/instrument/__init__test.py
+++ esis/science/papers/instrument/__init__test.py
@@ -1,7 +1,8 @@
 from . import document
 
 
-def test_document():
-    doc = document()
-    doc.generate_pdf()
-    doc.generate_tex()
+def test_document(capsys):
+    with capsys.disabled():
+        doc = document()
+        doc.generate_pdf()
+        doc.generate_tex()
