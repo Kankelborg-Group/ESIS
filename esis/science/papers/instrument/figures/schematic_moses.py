092c81d9b0e21eb45b56081fda55d07e67e571d7
Roy Smart <roytsmart@gmail.com>
2022-01-12 16:24:47 -0700

    formatting

--- esis/science/papers/instrument/figures/schematic_moses.py
+++ esis/science/papers/instrument/figures/schematic_moses.py
@@ -9,7 +9,8 @@ def figure() -> pylatex.Figure:
     result = pylatex.Figure(position='!ht')
     result.escape = False
     result.add_image('figures/old/MOSES_Schematic', width=kgpy.latex.columnwidth)
-    result.add_caption(pylatex.NoEscape(r"""
+    result.add_caption(pylatex.NoEscape(
+        r"""
 Schematic diagram of the \MOSES\ instrument.
 Incident light on the right forms an undispersed image on the central $m=0$ \CCD.
 Dispersed images are formed on the outboard $m=\pm1$ \CCDs."""
