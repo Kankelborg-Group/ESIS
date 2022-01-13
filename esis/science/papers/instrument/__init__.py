3ca52a85fa2dd84ab6f95f34b714b1eabcfdc101
Roy Smart <roytsmart@gmail.com>
2022-01-12 17:33:23 -0700

    Refactored the 'The ESIS Concept' section of esis.science.papers.instrument.document() to a new function, esis.science.papers.instrument.sections.esis_concept.section()

--- esis/science/papers/instrument/__init__.py
+++ esis/science/papers/instrument/__init__.py
@@ -938,22 +938,7 @@ def document() -> kgpy.latex.Document:
 
     doc.append(sections.introduction.body())
 
-    with doc.create(pylatex.Section(pylatex.NoEscape('The \ESIS\ Concept'))):
-        doc.append(pylatex.NoEscape(
-            r"""A primary goal of the \ESIS\ instrument is to improve upon the imaging spectroscopy 
-demonstrated by \MOSES.  
-Therefore, the design of the new instrument draws heavily from experiences and lessons learned through two flights of 
-the \MOSES\ instrument.
-\ESIS\ and \MOSES\ are both \CTIS\ instruments.
-As such, both produce \sout{dispersed images} \roy{overlappograms} of a narrow portion of the solar spectrum, with the goal of enabling the 
-reconstruction of a spectral line profile at every point in the field of view.
-The similarities end there, however, as the optical layout of \ESIS\ differs significantly from that of \MOSES.
-In this section, we detail some difficulties and limitations encountered with \MOSES, then describe how the new design 
-of \ESIS\ addresses these issues."""
-        ))
-
-        doc.append(sections.esis_concept.moses_limitations.subsection())
-        doc.append(sections.esis_concept.esis_features.subsection())
+    doc.append(sections.esis_concept.section())
 
     with doc.create(pylatex.Section('Science Objectives')):
         doc.append(pylatex.NoEscape(
