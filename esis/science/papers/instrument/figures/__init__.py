e42d17b27e6eb5c5189a9be2ec27294c3937cbf3
Roy Smart <roytsmart@gmail.com>
2022-01-13 21:07:46 -0700

    Moved esis.science.papers.instrument.figures.ccd_efficiency_vs_wavelength_pdf() to esis.science.papers.instrument.figures.ccd_efficiency_vs_wavelength.pdf()

--- esis/science/papers/instrument/figures/__init__.py
+++ esis/science/papers/instrument/figures/__init__.py
@@ -1,22 +1,3 @@
-import typing as typ
-import pathlib
-import matplotlib.figure
-import matplotlib.colors
-import matplotlib.lines
-import matplotlib.axes
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.spatial
-import astropy.units as u
-import astropy.visualization
-import kgpy.vector
-import kgpy.format
-import kgpy.plot
-import kgpy.transform
-import kgpy.optics
-import kgpy.grid
-import esis.optics
-from .. import optics as optics_factories
 from . import formatting
 from . import caching
 from . import schematic_moses
@@ -36,6 +17,7 @@ from . import grating_multilayer_schematic
 from . import grating_efficiency_vs_angle
 from . import component_efficiency_vs_wavelength
 from . import grating_efficiency_vs_position
+from . import ccd_efficiency_vs_wavelength
 
 __all__ = [
     'schematic_moses',
@@ -55,41 +37,5 @@ __all__ = [
     'grating_efficiency_vs_angle',
     'component_efficiency_vs_wavelength',
     'grating_efficiency_vs_position',
+    'ccd_efficiency_vs_wavelength',
 ]
-
-
-def ccd_efficiency_vs_wavelength() -> matplotlib.figure.Figure:
-    with astropy.visualization.quantity_support():
-        fig, ax = plt.subplots(
-            figsize=(formatting.column_width, 2.75),
-            constrained_layout=True,
-        )
-        wavelength = esis.optics.grating.efficiency.witness.vs_wavelength_g17()[2].to(u.AA)
-        rays = kgpy.optics.rays.Rays(
-            wavelength=wavelength,
-        )
-        ax.plot(
-            wavelength,
-            # wavelength,
-            kgpy.optics.surface.material.CCDStern1994().transmissivity(rays).to(u.percent),
-            label=r'Stern 1994',
-        )
-        ax.plot(
-            wavelength,
-            # wavelength,
-            kgpy.optics.surface.material.CCDStern2004().transmissivity(rays),
-            label=r'Stern 2004',
-        )
-        ax.add_artist(ax.legend())
-        lines = component_efficiency_vs_wavelength.annotate_wavelength(ax=ax)
-        ax.set_xlabel(f'wavelength ({ax.get_xlabel()})')
-        ax.set_ylabel(f'quantum efficiency ({ax.get_ylabel()})')
-        # ax.legend()
-        ax.legend(handles=lines, bbox_to_anchor=(0.5, -0.3), loc='upper center', ncol=2)
-
-        fig.set_constrained_layout_pads(h_pad=0.1)
-        return fig
-
-
-def ccd_efficiency_vs_wavelength_pdf() -> pathlib.Path:
-    return caching.cache_pdf(ccd_efficiency_vs_wavelength)
