98118fad5d52cc1dcbc1071ae1f0905c9c14f584
Roy Smart <roytsmart@gmail.com>
2022-01-14 01:35:15 -0700

    Moved vignetting figure from esis.science.papers.instrument.document() to esis.science.papers.instrument.figures.vignetting.figure()

--- esis/science/papers/instrument/figures/vignetting.py
+++ esis/science/papers/instrument/figures/vignetting.py
@@ -1,6 +1,8 @@
 import pathlib
 import matplotlib.figure
 import matplotlib.pyplot as plt
+import pylatex
+import kgpy.latex
 from .. import optics as optics_factories
 from . import formatting
 from . import caching
@@ -8,6 +10,7 @@ from . import caching
 __all__ = [
     'figure_mpl',
     'pdf',
+    'figure',
 ]
 
 
@@ -27,3 +30,18 @@ def figure_mpl() -> matplotlib.figure.Figure:
 
 def pdf() -> pathlib.Path:
     return caching.cache_pdf(figure_mpl)
+
+
+def figure() -> pylatex.Figure:
+    result = pylatex.Figure()
+    result.add_image(str(pdf()), width=None)
+    result.add_caption(pylatex.NoEscape(
+        r"""
+\roy{(Top) 2D histogram counting the number of rays that were unvignetted by the \ESIS\ optical 
+system as a function of field position.
+The count is normalized to the maximum number of unvignetted rays at any field point.
+The field and pupil grids have the same parameters as the grid for Figure~\ref{fig:spotSize}.
+(Bottom) Residual between the top histogram and the vignetting model described in Table~\ref{table:vignetting}}"""
+    ))
+    result.append(kgpy.latex.Label('fig:vignetting'))
+    return result
