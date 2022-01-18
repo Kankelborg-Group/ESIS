09e23152f2ffa168cd013cd3819243a67a9dca95
Roy Smart <roytsmart@gmail.com>
2022-01-18 16:17:16 -0700

    Updates from meeting with Charles.

--- esis/science/papers/instrument/tables/prescription.py
+++ esis/science/papers/instrument/tables/prescription.py
@@ -12,7 +12,7 @@ def table() -> pylatex.Table:
     with result.create(pylatex.Center()) as centering:
         with centering.create(pylatex.Tabular('lll')) as tabular:
             tabular.escape = False
-            tabular.add_row(['Surface', 'Parameter', 'Modeled value (measured value)'])
+            tabular.add_row(['Surface', 'Parameter', 'Calculated value (measured value)'])
             tabular.add_hline()
             tabular.add_row([r'Primary', r'Surface shape', r'Parabolic'])
             tabular.add_row([r'', r'Focal length ', r'\primaryFocalLength\ (\primaryFocalLengthMeasured)'])
