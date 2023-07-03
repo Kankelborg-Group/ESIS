f672e3a2d674b8fde8fc7f75baa2fb5f2c455c96
Roy Smart <roytsmart@gmail.com>
2023-07-03 09:47:33 -0600

    Updated `esis.optics.primary` to use `kgpy.labeled` arrays.

--- esis/optics/primary/__init__.py
+++ esis/optics/primary/__init__.py
@@ -3,73 +3,77 @@ import dataclasses
 import numpy as np
 import pandas
 import astropy.units as u
-from kgpy import Name, optics, format, mixin
+import kgpy.mixin
+import kgpy.format
+import kgpy.labeled
+import kgpy.uncertainty
+import kgpy.optics
 from . import efficiency
 
 __all__ = ['Primary']
 
-SurfaceT = optics.surface.Surface[
-    optics.surface.sag.Standard,
-    optics.surface.material.Mirror,
-    optics.surface.aperture.RegularPolygon,
-    optics.surface.aperture.RegularPolygon,
+SurfaceT = kgpy.optics.surfaces.Surface[
+    kgpy.optics.surfaces.sags.Standard,
+    kgpy.optics.surfaces.materials.Mirror,
+    kgpy.optics.surfaces.apertures.RegularPolygon,
+    kgpy.optics.surfaces.apertures.RegularPolygon,
     None,
 ]
 
-
-class PrimaryAxes(mixin.AutoAxis):
-    def __init__(self):
-        super().__init__()
-        self.primary_translation_x = self.auto_axis_index(from_right=False)
-        self.primary_translation_y = self.auto_axis_index(from_right=False)
-        self.primary_translation_z = self.auto_axis_index(from_right=False)
+#
+# class PrimaryAxes(mixin.AutoAxis):
+#     def __init__(self):
+#         super().__init__()
+#         self.primary_translation_x = self.auto_axis_index(from_right=False)
+#         self.primary_translation_y = self.auto_axis_index(from_right=False)
+#         self.primary_translation_z = self.auto_axis_index(from_right=False)
 
 
 @dataclasses.dataclass
-class Primary(optics.component.TranslationComponent[SurfaceT]):
-    name: Name = dataclasses.field(default_factory=lambda: Name('primary'))
-    radius: u.Quantity = np.inf * u.mm
-    conic: u.Quantity = -1 * u.dimensionless_unscaled
-    mtf_degradation_factor: u.Quantity = 0 * u.dimensionless_unscaled
-    slope_error: optics.surface.sag.SlopeErrorRMS = dataclasses.field(default_factory=optics.surface.sag.SlopeErrorRMS)
-    ripple: optics.surface.sag.RippleRMS = dataclasses.field(default_factory=optics.surface.sag.RippleRMS)
-    microroughness: optics.surface.sag.RoughnessRMS = dataclasses.field(default_factory=optics.surface.sag.RoughnessRMS)
+class Primary(kgpy.optics.components.TranslationComponent[SurfaceT]):
+    name: str = 'primary'
+    radius: kgpy.uncertainty.ArrayLike = np.inf * u.mm
+    conic: kgpy.uncertainty.ArrayLike = -1 * u.dimensionless_unscaled
+    mtf_degradation_factor: kgpy.uncertainty.ArrayLike = 0 * u.dimensionless_unscaled
+    slope_error: kgpy.optics.surfaces.sags.SlopeErrorRMS = dataclasses.field(default_factory=kgpy.optics.surfaces.sags.SlopeErrorRMS)
+    ripple: kgpy.optics.surfaces.sags.RippleRMS = dataclasses.field(default_factory=kgpy.optics.surfaces.sags.RippleRMS)
+    microroughness: kgpy.optics.surfaces.sags.RoughnessRMS = dataclasses.field(default_factory=kgpy.optics.surfaces.sags.RoughnessRMS)
     num_sides: int = 0
-    clear_half_width: u.Quantity = 0 * u.mm
-    border_width: u.Quantity = 0 * u.mm
-    material: optics.surface.material.MultilayerMirror = dataclasses.field(
-        default_factory=optics.surface.material.MultilayerMirror)
+    clear_half_width: kgpy.uncertainty.ArrayLike = 0 * u.mm
+    border_width: kgpy.uncertainty.ArrayLike = 0 * u.mm
+    material: kgpy.optics.surfaces.materials.MultilayerMirror = dataclasses.field(
+        default_factory=kgpy.optics.surfaces.materials.MultilayerMirror)
 
     @property
-    def focal_length(self) -> u.Quantity:
+    def focal_length(self) -> kgpy.uncertainty.ArrayLike:
         return self.radius / 2
 
     @property
-    def clear_radius(self) -> u.Quantity:
+    def clear_radius(self) -> kgpy.uncertainty.ArrayLike:
         return self.clear_half_width / np.cos(360 * u.deg / self.num_sides / 2)
 
     @property
-    def mech_radius(self) -> u.Quantity:
+    def mech_radius(self) -> kgpy.uncertainty.ArrayLike:
         return (self.clear_half_width + self.border_width) / np.cos(360 * u.deg / self.num_sides / 2)
 
     @property
-    def mech_half_width(self) -> u.Quantity:
+    def mech_half_width(self) -> kgpy.uncertainty.ArrayLike:
         return self.clear_half_width + self.border_width
 
     @property
     def surface(self) -> SurfaceT:
         surface = super().surface  # type: SurfaceT
-        surface.sag = optics.surface.sag.Standard(
+        surface.sag = kgpy.optics.surfaces.sags.Standard(
             radius=-self.radius,
             conic=self.conic,
         )
         surface.material = self.material.copy()
-        surface.aperture = optics.surface.aperture.RegularPolygon(
+        surface.aperture = kgpy.optics.surfaces.apertures.RegularPolygon(
             radius=self.clear_radius,
             num_sides=self.num_sides,
             # offset_angle=180 * u.deg / self.num_sides,
         )
-        surface.aperture_mechanical = optics.surface.aperture.RegularPolygon(
+        surface.aperture_mechanical = kgpy.optics.surfaces.apertures.RegularPolygon(
             radius=self.mech_radius,
             num_sides=self.num_sides,
             # offset_angle=180 * u.deg / self.num_sides,
@@ -77,15 +81,15 @@ class Primary(optics.component.TranslationComponent[SurfaceT]):
         return surface
 
     @property
-    def dataframe(self) -> pandas:
+    def dataframe(self) -> pandas.DataFrame:
         dataframe = super().dataframe
-        dataframe['radius'] = [format.quantity(self.radius.to(u.mm))]
-        dataframe['conic constant'] = [format.quantity(self.conic)]
-        dataframe['slope error'] = [format.quantity(self.slope_error.value)]
-        dataframe['ripple'] = [format.quantity(self.ripple.value)]
-        dataframe['microroughness'] = [format.quantity(self.microroughness.value)]
+        dataframe['radius'] = [kgpy.format.quantity(self.radius.to(u.mm))]
+        dataframe['conic constant'] = [kgpy.format.quantity(self.conic)]
+        dataframe['slope error'] = [kgpy.format.quantity(self.slope_error.value)]
+        dataframe['ripple'] = [kgpy.format.quantity(self.ripple.value)]
+        dataframe['microroughness'] = [kgpy.format.quantity(self.microroughness.value)]
         dataframe['number of sides'] = [self.num_sides]
-        dataframe['clear half-width'] = [format.quantity(self.clear_half_width.to(u.mm))]
-        dataframe['border width'] = [format.quantity(self.border_width.to(u.mm))]
-        dataframe['substrate thickness'] = [format.quantity(self.substrate_thickness.to(u.mm))]
+        dataframe['clear half-width'] = [kgpy.format.quantity(self.clear_half_width.to(u.mm))]
+        dataframe['border width'] = [kgpy.format.quantity(self.border_width.to(u.mm))]
+        dataframe['substrate thickness'] = [kgpy.format.quantity(self.material.thickness.to(u.mm))]
         return dataframe
--- esis/optics/primary/efficiency/model/__init__.py
+++ esis/optics/primary/efficiency/model/__init__.py
@@ -2,6 +2,9 @@ import typing as typ
 import pathlib
 import astropy.units as u
 import pandas
+import kgpy.labeled
+import kgpy.optics.vectors
+import kgpy.function
 
 __all__ = [
     'vs_wavelength',
@@ -12,11 +15,18 @@ base_path = pathlib.Path(__file__).parent
 file_vs_wavelength = base_path / 'SiC_2nmSiO2.dat'
 
 
-def vs_wavelength() -> typ.Tuple[u.Quantity, u.Quantity, u.Quantity]:
+def vs_wavelength() -> kgpy.function.Array[kgpy.optics.vectors.InputAngleVector, kgpy.labeled.Array]:
     df = pandas.read_table(file_vs_wavelength, sep='\t', header=2)
     arr = df.to_numpy()
-    angle_input = 0 * u.deg
-    wavelength = arr[..., 0] * u.nm
-    efficiency = arr[..., 1] * u.dimensionless_unscaled
-    return angle_input, wavelength, efficiency
+    angle_input = kgpy.labeled.Array([0] * u.deg, axes=['angle_input'])
+    wavelength = kgpy.labeled.Array(arr[..., 0] * u.nm, axes=['wavelength'])
+    efficiency = kgpy.labeled.Array(arr[..., 1] * u.dimensionless_unscaled, axes=['wavelength'])
+    return kgpy.function.Array(
+        input=kgpy.optics.vectors.InputAngleVector(
+            wavelength=wavelength,
+            angle_input_y=angle_input,
+            # angle_input=kgpy.optics.vectors.Spherical(y=angle_input),
+        ),
+        output=efficiency,
+    )
 
