3e3c1673f11310e134339905c594dbe1544d822a
Roy Smart <roytsmart@gmail.com>
2023-10-16 23:54:32 -0600

    Disabling imports temporarily to get things working.

--- esis/__init__.py
+++ esis/__init__.py
@@ -1,14 +1,7 @@
 """
 Package which provides a model of the ESIS instrument and classes for representing and analyzing ESIS observations.
 """
-__all__ = [
-    'optics',
-    'data',
-    'flight',
-    'science'
-]
-
 from . import optics
-from . import data
-from . import flight
-from . import science
+# from . import data
+# from . import flight
+# from . import science
