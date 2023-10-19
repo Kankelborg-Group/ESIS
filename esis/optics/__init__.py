9ca1eaf85131074caa42c6bcabbfec887bca672e
Roy Smart <roytsmart@gmail.com>
2023-10-19 14:35:44 -0600

    Added `esis.optics.Filter` class.

--- esis/optics/__init__.py
+++ esis/optics/__init__.py
@@ -7,4 +7,5 @@ from ._central_obscurations import *
 from ._primary_mirrors import PrimaryMirror
 from ._field_stops import *
 from ._gratings import *
+from ._filters import *
 from ._detectors import *
