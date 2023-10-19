5da91d940b896172631aa1be0b9fe4f87e5acb31
Roy Smart <roytsmart@gmail.com>
2023-10-19 05:09:32 -0600

    Added `esis.optics.Detector` class.

--- esis/optics/__init__.py
+++ esis/optics/__init__.py
@@ -6,3 +6,4 @@ from . import abc
 from ._central_obscurations import *
 from ._primary_mirrors import PrimaryMirror
 from ._gratings import *
+from ._detectors import *
--- esis/optics/abc.py
+++ esis/optics/abc.py
@@ -1,3 +1,4 @@
 from ._central_obscurations import AbstractCentralObscuration
 from ._primary_mirrors import AbstractPrimaryMirror
 from ._gratings import AbstractGrating
+from ._detectors import AbstractDetector
