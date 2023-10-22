a4a1622ca1c582384e32f01b8c3ade6ffd3eb391
Roy Smart <roytsmart@gmail.com>
2023-10-21 22:38:37 -0600

    Remove commented-out code

--- esis/__init__.py
+++ esis/__init__.py
@@ -2,6 +2,3 @@
 Package which provides a model of the ESIS instrument and classes for representing and analyzing ESIS observations.
 """
 from . import optics
-# from . import data
-# from . import flight
-# from . import science
