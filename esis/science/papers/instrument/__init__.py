5c9eb73340ad766137803852e4bb3344fb8cc3ad
Roy Smart <roytsmart@gmail.com>
2023-10-30 10:44:29 -0600

    Moved `esis.science.papers.instrument.document()` to `esis.science.papers.instrument._document.document()`

--- esis/science/papers/instrument/__init__.py
+++ esis/science/papers/instrument/__init__.py
@@ -1,58 +1,5 @@
-import pathlib
-import matplotlib.pyplot as plt
-import aastex
 from . import preamble
 from . import variables
 from . import authors
 from . import sections
-
-path_base = pathlib.Path(__file__).parent
-path_pdf = path_base / 'instrument'
-path_figures = path_base / 'figures'
-
-
-def document() -> aastex.Document:
-
-    plt.rcParams['text.usetex'] = True
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.size'] = 9
-    plt.rcParams['lines.linewidth'] = 1
-
-    doc = aastex.Document(
-        default_filepath=path_pdf,
-        documentclass='aastex631',
-        document_options=[
-            'twocolumn',
-            # 'linenumbers',
-        ]
-    )
-
-    doc.packages.append(aastex.Package('paralist'))
-    doc.packages.append(aastex.Package('amsmath'))
-    doc.packages.append(aastex.Package('acronym'))
-
-    doc.preamble += preamble.body()
-
-    variables.append_to_document(doc)
-
-    doc.append(aastex.Title('The EUV Snapshot Imaging Spectrograph'))
-
-    doc += authors.author_list()
-
-    # doc.append(sections.abstract.section())
-
-    doc.append(sections.introduction.section())
-
-    # doc.append(sections.esis_concept.section())
-    #
-    # doc.append(sections.science_objectives.section())
-    #
-    # doc.append(sections.esis_instrument.section(doc))
-    #
-    # doc.append(sections.mission_profile.section())
-    #
-    # doc.append(sections.conclusion.section())
-
-    doc.append(aastex.Bibliography("sources"))
-
-    return doc
+from ._document import *
