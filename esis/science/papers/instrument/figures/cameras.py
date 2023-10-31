0cb55bf02072da1f205372a40e8c275671611db6
Roy Smart <roytsmart@gmail.com>
2023-10-30 20:02:17 -0600

    Modified `esis.science.papers.instrument.figures.cameras` to use the `aastex` package.

--- esis/science/papers/instrument/figures/cameras.py
+++ esis/science/papers/instrument/figures/cameras.py
@@ -1,18 +1,16 @@
-import pylatex
-import kgpy.latex
+import aastex
 
 __all__ = [
     'figure',
 ]
 
 
-def figure() -> pylatex.Figure:
-    result = pylatex.Figure()
-    result.add_image('figures/old/ESIS_Cameras_1gr_text', width=kgpy.latex.columnwidth)
-    result.add_caption(pylatex.NoEscape(
+def figure() -> aastex.Figure:
+    result = aastex.Figure("F-cameras")
+    result.add_image('figures/old/ESIS_Cameras_1gr_text', width=aastex.column_width_inches)
+    result.add_caption(aastex.NoEscape(
         r"""
 \ESIS\ camera assembly as built by \MSFCShort.  
 Thin film filters and filter tubes are not installed in this image."""
     ))
-    result.append(kgpy.latex.Label('F-cameras'))
     return result
