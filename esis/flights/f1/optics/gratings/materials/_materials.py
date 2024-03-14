5f36952101df8d078013b229a703f856736ab2c0
Roy Smart <roytsmart@gmail.com>
2024-03-14 15:33:03 -0600

    Added `esis.flights.f1.optics.gratings.materials.multilayer_witness_measured()`.

--- esis/flights/f1/optics/gratings/materials/_materials.py
+++ esis/flights/f1/optics/gratings/materials/_materials.py
@@ -1,9 +1,13 @@
 import dataclasses
+import pathlib
+import numpy as np
 import astropy.units as u
+import named_arrays as na
 import optika
 
 __all__ = [
     "multilayer_design",
+    "multilayer_witness_measured",
 ]
 
 
@@ -121,3 +125,97 @@ def multilayer_design() -> optika.materials.MultilayerMirror:
         ],
         substrate=layer_sio2,
     )
+
+
+def multilayer_witness_measured() -> optika.materials.MeasuredMirror:
+    """
+    A reflectivity measurement of the witness samples associated with the ESIS
+    diffraction gratings gathered by Eric Gullikson.
+
+    Examples
+    --------
+    Load the measurement and plot it as a function of wavelength.
+
+    .. jupyter-execute::
+
+        import matplotlib.pyplot as plt
+        import named_arrays as na
+        from esis.flights.f1.optics import gratings
+
+        # Load the witness sample measurements
+        multilayer = gratings.materials.multilayer_witness_measured()
+        measurement = multilayer.efficiency_measured
+
+        # Plot the measurement as a function of wavelength
+        fig, ax = plt.subplots(constrained_layout=True)
+        na.plt.plot(
+            measurement.inputs.wavelength,
+            measurement.outputs,
+            ax=ax,
+            axis="wavelength",
+            label=multilayer.serial_number,
+        )
+        ax.set_xlabel(f"wavelength ({measurement.inputs.wavelength.unit:latex_inline})");
+        ax.set_ylabel("reflectivity");
+        ax.legend();
+    """
+    path_base = pathlib.Path(__file__).parent / "_data"
+
+    wavelength_17, reflectivity_17 = np.loadtxt(
+        fname=path_base / "Witness_g17.txt",
+        skiprows=1,
+        unpack=True,
+    )
+    wavelength_19, reflectivity_19 = np.loadtxt(
+        fname=path_base / "Witness_g19.txt",
+        skiprows=1,
+        unpack=True,
+    )
+    wavelength_24, reflectivity_24 = np.loadtxt(
+        fname=path_base / "Witness_g24.txt",
+        skiprows=1,
+        unpack=True,
+    )
+
+    wavelength_17 = na.ScalarArray(wavelength_17, axes="wavelength")
+    wavelength_19 = na.ScalarArray(wavelength_19, axes="wavelength")
+    wavelength_24 = na.ScalarArray(wavelength_24, axes="wavelength")
+
+    reflectivity_17 = na.ScalarArray(reflectivity_17, axes="wavelength")
+    reflectivity_19 = na.ScalarArray(reflectivity_19, axes="wavelength")
+    reflectivity_24 = na.ScalarArray(reflectivity_24, axes="wavelength")
+
+    wavelength = na.stack(
+        arrays=[wavelength_17, wavelength_19, wavelength_24],
+        axis="channel",
+    )
+    reflectivity = na.stack(
+        arrays=[reflectivity_17, reflectivity_19, reflectivity_24],
+        axis="channel",
+    )
+
+    wavelength = (wavelength << u.nm) << u.AA
+
+    serial_number = [
+        "UBO-16-017",
+        "UBO-16-019",
+        "UBO-16-024",
+    ]
+    serial_number = np.array(serial_number)
+    serial_number = na.ScalarArray(serial_number, axes="channel")
+
+    result = optika.materials.MeasuredMirror(
+        efficiency_measured=na.FunctionArray(
+            inputs=na.SpectralDirectionalVectorArray(
+                wavelength=wavelength,
+                direction=4 * u.deg,
+            ),
+            outputs=reflectivity,
+        ),
+        substrate=optika.materials.Layer(
+            chemical="Si",
+        ),
+        serial_number=serial_number,
+    )
+
+    return result
--- esis/flights/f1/optics/gratings/materials/_materials_test.py
+++ esis/flights/f1/optics/gratings/materials/_materials_test.py
@@ -5,3 +5,8 @@ import esis
 def test_multilayer_design():
     r = esis.flights.f1.optics.gratings.materials.multilayer_design()
     assert isinstance(r, optika.materials.AbstractMultilayerMirror)
+
+
+def test_multilayer_witness_measured():
+    r = esis.flights.f1.optics.gratings.materials.multilayer_witness_measured()
+    assert isinstance(r, optika.materials.MeasuredMirror)
