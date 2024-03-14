7dc23d67087dae0ec89ccea6c8164c96610d094b
Roy Smart <roytsmart@gmail.com>
2024-03-13 22:36:45 -0600

    Renamed `esis.flights.f1.optics.primaries.materials.multilayer_witness()` to `multilayer_witness_fit()`.

--- esis/flights/f1/optics/primaries/materials/_materials_test.py
+++ esis/flights/f1/optics/primaries/materials/_materials_test.py
@@ -12,8 +12,8 @@ def test_multilayer_witness_measured():
     assert isinstance(r, optika.materials.MeasuredMirror)
 
 
-def test_multilayer_witness():
-    r = esis.flights.f1.optics.primaries.materials.multilayer_witness()
+def test_multilayer_witness_fit():
+    r = esis.flights.f1.optics.primaries.materials.multilayer_witness_fit()
     assert isinstance(r, optika.materials.MultilayerMirror)
 
 
