9f0b221e79673a2b343d836eeceb754d27c7a13e
jacobdparker <jacobdparker@gmail.com>
2025-04-30 16:55:20 -0400

    Cleaning up notebook and capturing optimized ESIS-II high resolution as models outside notebook.

--- esis/flights/f2/optics/models.py
+++ esis/flights/f2/optics/models.py
@@ -197,6 +197,93 @@ def design_high_resolution(
 
     return result
 
+def design_high_resolution_single(
+    grid: None | optika.vectors.ObjectVectorArray = None,
+    num_distribution: int = 11,
+) -> esis.optics.OpticsModel:
+    """
+    Single channel of high resolution ESIS2 optical design that uses entire optical bench length for increased resolution.
+
+    Parameters
+    ----------
+    grid
+        sampling of wavelength, field, and pupil positions that will be used to
+        characterize the optical system.
+    num_distribution
+        number of Monte Carlo samples to draw when computing uncertainties
+
+    Examples
+    --------
+    Plot the rays traveling through the optical system, as viewed from the front.
+
+    .. jupyter-execute::
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import astropy.units as u
+        import astropy.visualization
+        import named_arrays as na
+        import optika
+        import esis
+
+        grid = optika.vectors.ObjectVectorArray(
+            wavelength=na.linspace(-1, 1, axis="wavelength",  num=2) / 2,
+            field=0.99 * na.Cartesian2dVectorLinearSpace(
+                start=-1,
+                stop=1,
+                axis=na.Cartesian2dVectorArray("field_x", "field_y"),
+                num=5,
+            ),
+            pupil=na.Cartesian2dVectorLinearSpace(
+                start=-1,
+                stop=1,
+                axis=na.Cartesian2dVectorArray("pupil_x", "pupil_y"),
+                num=5,
+            ),
+        )
+
+        model = esis.flights.f2.optics.models.design_proposed(
+            grid=grid,
+            num_distribution=0,
+        )
+
+        with astropy.visualization.quantity_support():
+            fig, ax = plt.subplots(
+                figsize=(6, 6.5),
+                constrained_layout=True
+            )
+            ax.set_aspect("equal")
+            model.system.plot(
+                components=("x", "y"),
+                color="black",
+                kwargs_rays=dict(
+                    color=na.ScalarArray(np.array(["tab:orange", "tab:blue"]), axes="wavelength"),
+                    label=model.system.grid_input.wavelength.astype(int),
+                ),
+            );
+            handles, labels = ax.get_legend_handles_labels()
+            labels = dict(zip(labels, handles))
+            fig.legend(labels.values(), labels.keys());
+    """
+    result = design_high_resolution(
+        grid=grid,
+        num_distribution=num_distribution,
+    )
+
+    index = dict(channel=0)
+
+    result.grating.azimuth = result.grating.azimuth[index]
+    result.filter.azimuth = result.filter.azimuth[index]
+
+    result.detector.name_channel = result.detector.name_channel[index]
+    result.detector.azimuth = result.detector.azimuth[index]
+
+    result.roll = -result.grating.azimuth
+
+    return result
+
+
+
 def design_proposed_single(
     grid: None | optika.vectors.ObjectVectorArray = None,
     num_distribution: int = 11,
