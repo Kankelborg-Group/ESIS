62cb4c082b9671b6011e1898f3aa641a8da2e508
Roy Smart <roytsmart@gmail.com>
2025-07-24 10:41:36 -0600

    big improvements

--- esis/flights/f1/__init__.py
+++ esis/flights/f1/__init__.py
@@ -1,10 +1,11 @@
 """
-Models and data associated with the first flight of the ESIS instrument.
+Models and data associated with the first flight of the ESIS instrument in 2019.
 """
-
-from . import optics
-from . import timeline
 
 __all__ = [
     "optics",
+    "timeline",
 ]
+
+from . import optics
+from ._timeline import timeline
---
+++ esis/flights/f1/_timeline/_timeline_test.py
@@ -0,0 +1,12 @@
+import dataclasses
+import astropy.units as u
+import esis
+
+
+def test_timeline():
+    result = esis.flights.f1.timeline()
+
+    assert isinstance(result, esis.nsroc.Timeline)
+
+    for f in dataclasses.fields(result):
+        assert getattr(result, f.name) > 0 * u.s
