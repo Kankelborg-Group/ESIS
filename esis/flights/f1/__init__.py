6ff0491de9c9cc1ee7a1b6912f2dd0c19f0296e9
Roy Smart <roytsmart@gmail.com>
2025-07-28 15:55:55 -0600

    Added `esis.flights.f1.path_fits()` function.

--- esis/flights/f1/__init__.py
+++ esis/flights/f1/__init__.py
@@ -2,10 +2,14 @@
 Models and data associated with the first flight of the ESIS instrument in 2019.
 """
 
+from . import optics
+from ._timeline import timeline
+from ._data import (
+    path_fits,
+)
+
 __all__ = [
     "optics",
     "timeline",
+    "path_fits",
 ]
-
-from . import optics
-from ._timeline import timeline
---
+++ esis/flights/f1/_data/__init__.py
@@ -0,0 +1,8 @@
+"""
+Inversion pipeline for the 2019 flight.
+"""
+from ._fits import path_fits
+
+__all__ = [
+    "path_fits",
+]
