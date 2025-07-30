e9a30eb15701c9ca3d4dc3bfea1834a4a86bb7cc
Roy Smart <roytsmart@gmail.com>
2025-07-30 14:30:38 -0600

    Added `esis.flights.f1.data.level_0()` function to load the 2019 Level-0 dataset.

--- docs/make.bat
+++ docs/make.bat
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b593cd1da19314a43db9a0854f6f92f2c59217571502da3321f9498c02fa16c8
-size 795
+oid sha256:bcf205ef2f3026bb71a0ed44d71afef0519162e1854d66bbb782dc864e3a05d7
+size 803
--- esis/flights/f1/data/__init__.py
+++ esis/flights/f1/data/__init__.py
@@ -3,7 +3,9 @@ Inversion pipeline for the 2019 flight.
 """
 
 from ._fits import path_fits
+from ._level_0 import level_0
 
 __all__ = [
     "path_fits",
+    "level_0",
 ]
